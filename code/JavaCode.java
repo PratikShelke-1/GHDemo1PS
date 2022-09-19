bd3efc6e7bb5297d259ffb15ab3bd2a302363812
ankush.ikhar <L00171111@atu.ie>
2022-09-19 12:54:06 +0100

    done

--- code/JavaCode.java
+++ code/JavaCode.java
@@ -1,6 +1,7 @@
-public class HelloLYIT {
+ipublic class HelloLYIT {
  public static void main(String[] args) {
  // Prints "Hello, LYIT from Pratik" as output
  System.out.println("Hello, LYIT from Pratik");
  }
 }
+---//Ankush
