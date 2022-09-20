260241cb1651d13bacdad1b07d1b37581e33bdcc
shashanksanap99 <L00171105@atu.ie>
2022-09-20 01:12:23 +0100

    Code Changes added to the repo

--- code/JavaCode.java
+++ code/JavaCode.java
@@ -2,6 +2,7 @@ ipublic class HelloLYIT {
  public static void main(String[] args) {
  // Prints "Hello, LYIT from Pratik" as output
  System.out.println("Hello, LYIT from Pratik");
- }
+ System.out.println("Hello, Shashank Here"); // Shashank changes
+}
 }
 ---//Ankush
